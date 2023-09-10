66d1164b542364877165d67f55bdbfb7fe896089
trinhtam <trinhtam2001@gmail.com>
2023-09-10 23:58:01 +0700

    init

---
+++ Final/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public async Task Start()
+    {
+        HttpClient client = new HttpClient();
+        using HttpResponseMessage response = await client.GetAsync("https://localhost:7134/WeatherForecast");
+        response.EnsureSuccessStatusCode();
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var obj = JsonConvert.DeserializeObject<User>(responseBody);
+
+        //WeatherForecast a = JsonUtility.FromJson<WeatherForecast>(responseBody);
+        Debug.Log("JSON response1: " + obj.FirstName.ToString());
+    }
+
+    // Update is called once per frame
+    
+
+    public class User
+    {
+        public string FirstName { get; set; }
+    }
+}
